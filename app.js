557ca0f63e5ecc44356623b56c153b3193a69b9c
Nazmul Kabir <nazmulkabirtushar@gmail.com>
2023-05-06 01:11:26 +0600

    fully done with dark mode

--- app.js
+++ app.js
@@ -4,3 +4,21 @@ const menu = document.querySelector('#menu');
 hamburger.addEventListener('click', () => {
   menu.classList.toggle('hidden');
 });
+
+
+// dark mode
+const moon = document.querySelector('#moon');
+const sun = document.querySelector('#sun');
+const body = document.querySelector('body');
+
+moon.addEventListener('click', () => {
+  body.classList.add('dark');
+  moon.classList.add('hidden');
+  sun.classList.remove('hidden');
+});
+
+sun.addEventListener('click', () => {
+  body.classList.remove('dark');
+  sun.classList.add('hidden');
+  moon.classList.remove('hidden');
+});
